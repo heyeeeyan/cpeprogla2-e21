7d2f8485ae97c565072f47c15c2ab8c45de5d03e
Ian Norril Venezuela <nailirron@gmail.com>
2016-10-08 08:50:33 +0800

    Add files via upload

---
+++ euler3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    ifstream f("input.txt");
+    FILE *fp;
+    fp = fopen("input.txt", "r");
+    
+    if(!fp){
+    	cout << "Cannot open file.\n";
+		system("pause");
+		exit(1);
+	}
+    
+    int i, num, sum=0;
+    while (f >> num) {
+    }
+    for (i=0; i<num;i++){
+    	if(i%3 == 0|| i%5== 0){
+    		sum+=i;
+		}
+	}  
+	cout << sum << endl;  
+    system("pause");
+    return 0;
+}   
